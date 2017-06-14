308bb1eaf986dbb9e683f567aefdfb46a36a2808
Sebastian Duarte <sebduart@cobogpgp7292.global.publicisgroupe.net>
2017-06-14 10:55:42 -0500

    Stage 1, first digit operators DOM working

--- script/calculator.js
+++ script/calculator.js
@@ -1,6 +1,44 @@
+function calculator() {
+    var digits, isDot, operator, textToDisplayX, textToDisplayY;
 
-document.getElementById('b1').addEventListener('click', pushKey);
+    digits = [1, 2, 3,4,5,6,7,8,9,0];
+    isDot = false;
+    operator = false;
+    textToDisplayX = "";
+    textToDisplayY = "";
 
-function pushKey() {
-    console.log(document.getElementById('b1').innerHTML);
+    for (var i = 0; i < digits.length; i++) {
+        document.getElementById('digit-' + digits[i]).addEventListener('click', clickDigit);
+        document.getElementById('digit-' + digits[i]).addEventListener('keydown', pushDigit);
+    }
+    
+    document.getElementById('digit-dot').addEventListener('click', pushDot);
+    //document.getElementById('digit-dot').addEventListener('click', pushDot);
+
+    function clickDigit() {
+        if (this.id === 'digit-0' && (textToDisplayX === "" || textToDisplayX === "0")) {
+            textToDisplayX = "0";
+        } else {
+            if (textToDisplayX.length > 0 && textToDisplayX[0] === '0' && textToDisplayX[1] != '.') {
+                textToDisplayX = textToDisplayX.slice(1, textToDisplayX.length);
+            }
+
+            textToDisplayX += parseInt(this.id[6]);
+        }
+
+        document.getElementById('display').innerHTML = '<span>' + textToDisplayX + '</span>';
+    }
+
+    function pushDigit() {
+        console.log('for keyboard events');
+    }
+
+    function pushDot() {
+        isDot = true;
+        textToDisplayX += '.';
+        document.getElementById('display').innerHTML = '<span>' + textToDisplayX + '</span>';
+        document.getElementById('digit-dot').removeEventListener('click', pushDot);
+    }
 }
+
+calculator();
