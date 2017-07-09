2f34e7ce8a2609a27c69a473172c8e46e91376e2
Sebastian Duarte <sebduart@cobogpgp7292.global.publicisgroupe.net>
2017-07-09 03:26:55 -0500

    function for dot done

--- script/calculator.js
+++ script/calculator.js
@@ -1,51 +1,96 @@
 function calculator() {
     var 
+    CONSTANTS,
     counter,
     digits,
-    ids,
-    tags,
+    isDot,
     textToDisplay;
 
-    digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-    ids = {
-        'clear': 'clear',
-        'digit': 'digit-',
-        'display': 'display'
+    CONSTANTS = {
+        'DIGITS': [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+        'IDS': {
+            'CLEAR': 'clear',
+            'DIGIT': 'digit-',
+            'DIGITDOT': 'digit-dot',
+            'DISPLAY': 'display'
+        },
+        'MARKERS': {
+            'CLICK': 'click',
+            'KEY': 'keydown'
+        },
+        'TAGS': {
+            'SPANO': '<span>',
+            'SPANX': '</span>'
+            
+        }
     };
-    markers = {
-        'click': 'click',
-        'key': 'keydown'
-    };
-    tags = {
-        'spanX': '</span>',
-        'spanO': '<span>'
-    };
+    counter = 0;
+    isDot = false;
     textToDisplay = "";
 
     function setEvents () {
-        for (counter = 0; counter < digits.length; counter++) {
-            document.getElementById(ids.digit + digits[counter]).
-                addEventListener(markers.click, digitEvent);
-            document.getElementById(ids.digit + digits[counter]).
-                addEventListener(markers.markerKey, digitEvent);
+        for (counter = 0; counter < CONSTANTS.DIGITS.length; counter++) {
+            document.getElementById(CONSTANTS.IDS.DIGIT + CONSTANTS.DIGITS[counter]).
+                addEventListener(CONSTANTS.MARKERS.CLICK, digitEvent);
+            document.getElementById(CONSTANTS.IDS.DIGIT + CONSTANTS.DIGITS[counter]).
+                addEventListener(CONSTANTS.MARKERS.KEY, digitEvent);
         }
 
-        document.getElementById(ids.clear).addEventListener(markers.click, clearEvent);
+        document.getElementById(CONSTANTS.IDS.CLEAR).
+            addEventListener(CONSTANTS.MARKERS.CLICK, clearEvent);
+        document.getElementById(CONSTANTS.IDS.CLEAR).
+            addEventListener(CONSTANTS.MARKERS.KEY, clearEvent);
+
+        document.getElementById(CONSTANTS.IDS.DIGITDOT).
+            addEventListener(CONSTANTS.MARKERS.CLICK, dotEvent);
+        document.getElementById(CONSTANTS.IDS.DIGITDOT).
+            addEventListener(CONSTANTS.MARKERS.KEY, dotEvent);
+    }
+
+    function clearEvent () {
+        isDot = false;
+        textToDisplay = "";
+        refreshDisplay(textToDisplay);
+        document.getElementById(CONSTANTS.IDS.DIGITDOT).
+            addEventListener(CONSTANTS.MARKERS.CLICK, dotEvent);
+        document.getElementById(CONSTANTS.IDS.DIGITDOT).
+            addEventListener(CONSTANTS.MARKERS.KEY, dotEvent);
+
+        return textToDisplay;
     }
 
     function digitEvent () {
         textToDisplay += parseInt(this.id[6]);
-        document.getElementById(ids.display).innerHTML = tags.spanO + textToDisplay + tags.spanX;
+        refreshDisplay(textToDisplay);
         
         return textToDisplay;
     }
 
-    function clearEvent () {
-        textToDisplay = "";
-        document.getElementById(ids.display).innerHTML = tags.spanO + textToDisplay + tags.spanX;
+    function dotEvent () {
+        if (isDot === false) {
+            isDot = true;
+        }
 
-        return textToDisplay;
+        textToDisplay += '.';
+        refreshDisplay(textToDisplay);
+        document.getElementById(CONSTANTS.IDS.DIGITDOT).
+            removeEventListener(CONSTANTS.MARKERS.CLICK, dotEvent);
+        document.getElementById(CONSTANTS.IDS.DIGITDOT).
+            removeEventListener(CONSTANTS.MARKERS.KEY, dotEvent);
+
+        return textToDisplay, isDot;
     }
+
+    function lessEvent () {}
+
+    function plusEvent () {}
+
+    function refreshDisplay (number) {
+        document.getElementById(CONSTANTS.IDS.DISPLAY).innerHTML = 
+            CONSTANTS.TAGS.SPANO + number + CONSTANTS.TAGS.SPANX;
+    }
+
+    function timesEvent () {}
 
     setEvents();
 }
