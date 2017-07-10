15045363c0b164b6f82d3ce85eb0a70a3b525f84
Sebastian Duarte <sebduart@cobogpgp7292.global.publicisgroupe.net>
2017-07-10 18:02:56 -0500

    Working on Zero contingency

--- script/calculator.js
+++ script/calculator.js
@@ -9,7 +9,7 @@ function calculator() {
     CONSTANTS = {
         'DIGITS': [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
         'IDS': {
-            'CLEAR': 'clear',
+            'CLEARCURRENT': 'clear-current',
             'DIGIT': 'digit-',
             'DIGITDOT': 'digit-dot',
             'DISPLAY': 'display'
@@ -26,40 +26,40 @@ function calculator() {
     };
     counter = 0;
     isDot = false;
+    isZero = false;
     textToDisplay = "";
 
     function setEvents () {
         for (counter = 0; counter < CONSTANTS.DIGITS.length; counter++) {
-            document.getElementById(CONSTANTS.IDS.DIGIT + CONSTANTS.DIGITS[counter]).
-                addEventListener(CONSTANTS.MARKERS.CLICK, digitEvent);
-            document.getElementById(CONSTANTS.IDS.DIGIT + CONSTANTS.DIGITS[counter]).
-                addEventListener(CONSTANTS.MARKERS.KEY, digitEvent);
+            ael(CONSTANTS.IDS.DIGIT + CONSTANTS.DIGITS[counter], CONSTANTS.MARKERS.CLICK, digitEvent);
+            ael(CONSTANTS.IDS.DIGIT + CONSTANTS.DIGITS[counter], CONSTANTS.MARKERS.KEY, digitEvent);
         }
 
-        document.getElementById(CONSTANTS.IDS.CLEAR).
-            addEventListener(CONSTANTS.MARKERS.CLICK, clearEvent);
-        document.getElementById(CONSTANTS.IDS.CLEAR).
-            addEventListener(CONSTANTS.MARKERS.KEY, clearEvent);
+        ael(CONSTANTS.IDS.CLEARCURRENT, CONSTANTS.MARKERS.CLICK, clearEvent);
+        ael(CONSTANTS.IDS.CLEARCURRENT, CONSTANTS.MARKERS.KEY, clearEvent);
 
-        document.getElementById(CONSTANTS.IDS.DIGITDOT).
-            addEventListener(CONSTANTS.MARKERS.CLICK, dotEvent);
-        document.getElementById(CONSTANTS.IDS.DIGITDOT).
-            addEventListener(CONSTANTS.MARKERS.KEY, dotEvent);
+        ael(CONSTANTS.IDS.DIGITDOT, CONSTANTS.MARKERS.CLICK, dotEvent);
+        ael(CONSTANTS.IDS.DIGITDOT, CONSTANTS.MARKERS.KEY, dotEvent);
     }
 
     function clearEvent () {
         isDot = false;
         textToDisplay = "";
         refreshDisplay(textToDisplay);
-        document.getElementById(CONSTANTS.IDS.DIGITDOT).
-            addEventListener(CONSTANTS.MARKERS.CLICK, dotEvent);
-        document.getElementById(CONSTANTS.IDS.DIGITDOT).
-            addEventListener(CONSTANTS.MARKERS.KEY, dotEvent);
+
+        ael(CONSTANTS.IDS.DIGITDOT, CONSTANTS.MARKERS.CLICK, dotEvent);
+        ael(CONSTANTS.IDS.DIGITDOT, CONSTANTS.MARKERS.KEY, dotEvent);
 
         return textToDisplay;
     }
 
     function digitEvent () {
+        var digitCero = CONSTANTS.IDS.DIGIT + "0";
+
+        if(this.id === digitCero && isZero) {
+            textToDisplay = zeroDigitEvent(textToDisplay);
+        }
+
         textToDisplay += parseInt(this.id[6]);
         refreshDisplay(textToDisplay);
         
@@ -69,28 +69,41 @@ function calculator() {
     function dotEvent () {
         if (isDot === false) {
             isDot = true;
-        }
 
-        textToDisplay += '.';
-        refreshDisplay(textToDisplay);
-        document.getElementById(CONSTANTS.IDS.DIGITDOT).
-            removeEventListener(CONSTANTS.MARKERS.CLICK, dotEvent);
-        document.getElementById(CONSTANTS.IDS.DIGITDOT).
-            removeEventListener(CONSTANTS.MARKERS.KEY, dotEvent);
+            textToDisplay += '.';
+            refreshDisplay(textToDisplay);
+
+            rel(CONSTANTS.IDS.DIGITDOT, CONSTANTS.MARKERS.CLICK, dotEvent);
+            rel(CONSTANTS.IDS.DIGITDOT, CONSTANTS.MARKERS.KEY, dotEvent);
+        }
 
         return textToDisplay, isDot;
     }
+
+    function divEvent () {}
 
     function lessEvent () {}
 
     function plusEvent () {}
 
-    function refreshDisplay (number) {
-        document.getElementById(CONSTANTS.IDS.DISPLAY).innerHTML = 
-            CONSTANTS.TAGS.SPANO + number + CONSTANTS.TAGS.SPANX;
+    function timesEvent () {}
+
+    function zeroDigitEvent (toCheck) {
+
+        return toCheck;
     }
 
-    function timesEvent () {}
+    function ael (who, what, where) {
+        document.getElementById(who).addEventListener(what, where);
+    }
+
+    function rel (who, what, where) {
+        document.getElementById(who).removeEventListener(what, where);
+    }
+
+    function refreshDisplay (textToRefresh) {
+        document.getElementById(CONSTANTS.IDS.DISPLAY).innerHTML = CONSTANTS.TAGS.SPANO + textToRefresh + CONSTANTS.TAGS.SPANX;
+    }
 
     setEvents();
 }
